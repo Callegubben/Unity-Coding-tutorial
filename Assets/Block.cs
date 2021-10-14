5915ba616b77f571ea61b3d2915a54aa76617240
Calle Turesson <37700533+Callegubben@users.noreply.github.com>
2021-10-14 15:44:19 +0200

    The breaking of blocks is now possible

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,27 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     public float hitPoints;
+    public GameManagerBlockBreaker gameManager;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            TakeDamage();
+        }
+    }
+
+    private void TakeDamage()
+    {
+        hitPoints--;
+        if (hitPoints <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        gameObject.SetActive(false);
     }
 }
